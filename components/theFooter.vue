5c642f790878069bc1c8f69dabf242e55c3ed876
aurictorkornoo <44117263+Auricdhero@users.noreply.github.com>
2023-09-08 12:00:07 +0000

    working on the landing page

--- components/theFooter.vue
+++ components/theFooter.vue
@@ -1,17 +1,35 @@
 <template>
     <div>
-        <v-footer class="d-flex flex-column">
-            <div class="bg-white d-flex w-100 align-center px-4">
-                <strong>Get connected with us on social networks!</strong>
-
-                <v-spacer></v-spacer>
-
-                <!-- <v-btn v-for="icon in icons" :key="icon" class="mx-4" :icon="icon" variant="plain" size="small"></v-btn> -->
+        <v-footer class="light-blue-lighten-3 text-center d-flex flex-column">
+            <div>
+                <v-btn v-for="icon in icons" :key="icon" class="mx-4" :icon="icon" variant="text"></v-btn>
             </div>
 
-            <div class="px-4 py-2 bg-light-blue-lighten-2 text-center w-100">
-                {{ new Date().getFullYear() }} — <strong>Built by Chain Medics</strong>
+            <div class="pt-0">
+                Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum. Praesent
+                ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet
+                dui. Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum
+                ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci
+                varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+            </div>
+
+            <v-divider></v-divider>
+
+            <div>
+                {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
             </div>
         </v-footer>
     </div>
 </template>
+<script>
+export default {
+    data: () => ({
+        icons: [
+            'mdi-facebook',
+            'mdi-twitter',
+            'mdi-linkedin',
+            'mdi-instagram',
+        ],
+    }),
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,243 @@
 <template>
     <div class="home mt-16 pt-16">
-        <v-row>
-            <v-col>
-                <v-container>
-                    <h2 class="text-center text-uppercase">welcome to chain medics</h2>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, tenetur molestiae. Magni,
-                        dignissimos.
-                        Hic ullam ut at sed distinctio nam a eos maxime, vitae, sint aliquid dolorem aperiam ipsam cumque.
-                    </p>
-                    <br>
-                    <v-row>
-                        <v-col cols="3">
-                            <v-btn to="/start">Start Now</v-btn>
-                        </v-col>
-                        <v-col cols="3">
-                            <v-btn>About</v-btn>
-                        </v-col>
-                    </v-row>
-                </v-container>
-            </v-col>
-            <v-col>
-                <img src="~/assets/landing1.png" height="400" alt="">
-            </v-col>
-        </v-row>
+        <v-container>
+            <v-row>
+                <v-col cols="8">
+                    <v-container>
+                        <v-sheet class="d-flex align-center justify-center flex-wrap text-center mx-auto px-4" width="550">
+                            <h2 class="text-center text-uppercase">welcome to chain medics</h2>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, tenetur molestiae. Magni,
+                                dignissimos.
+                                Hic ullam ut at sed distinctio nam a eos maxime, vitae, sint aliquid dolorem aperiam ipsam
+                                cumque.
+                            </p>
+                            <br />
+                            <v-row>
+                                <v-col cols="6">
+                                    <v-btn to="/start">Start Now</v-btn>
+                                </v-col>
+                                <v-col cols="6">
+                                    <v-btn>About</v-btn>
+                                </v-col>
+                            </v-row>
+                        </v-sheet>
+                    </v-container>
+                </v-col>
+                <v-col cols="4">
+                    <img src="~/assets/landing1.png" height="400" alt="">
+                </v-col>
+            </v-row>
+            <div class="grey-lighten-5 ">
+                <v-row class="mx-auto">
+                    <v-col>
+                        <img class="spons-logo" src="~/assets/buidlguidl.png" alt="">
+                    </v-col>
+                    <v-col>
+                        <img class="spons-logo" src="~/assets/celo.png" alt="">
+                    </v-col>
+                    <v-col>
+                        <img class="spons-logo" src="~/assets/Mazzuma.png" alt="">
+                    </v-col>
+                    <v-col>
+                        <img class="spons-logo" src="~/assets/remix.png" alt="">
+                    </v-col>
+                    <v-col>
+                        <img class="spons-logo" src="~/assets/scaffoldEth.png" alt="">
+                    </v-col>
+                    <v-col>
+                        <img class="spons-logo" src="~/assets/taikai.png" alt="">
+                    </v-col>
+                    <v-col>
+                        <img class="spons-logo" src="~/assets/celo.png" alt="">
+                    </v-col>
+                </v-row>
+            </div>
+
+            <div>
+                <v-row class="ml-1">
+                    <v-col cols="4">
+                        <v-card class="mx-auto my-12" max-width="374">
+                            <img src="~/assets/card1.jpg" height="250" cover alt="">
+                            <v-card-item>
+                                <v-card-title>
+                                    Features of Chain Medics
+                                </v-card-title>
+                                <v-card-text>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed quod corrupti iste. Ad sunt
+                                    eveniet cupiditate similique dicta quia enim amet placeat, iusto cumque perspiciatis
+                                    deleniti recusandae quibusdam voluptatum excepturi.
+                                </v-card-text>
+                            </v-card-item>
+                        </v-card>
+                    </v-col>
+                    <v-col cols="4">
+                        <v-card class="mx-auto my-12" max-width="374">
+                            <img src="~/assets/card1.jpg" height="250" cover alt="">
+                            <v-card-item>
+                                <v-card-title>
+                                    Features of Chain Medics
+                                </v-card-title>
+                                <v-card-text>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed quod corrupti iste. Ad sunt
+                                    eveniet cupiditate similique dicta quia enim amet placeat, iusto cumque perspiciatis
+                                    deleniti recusandae quibusdam voluptatum excepturi.
+                                </v-card-text>
+                            </v-card-item>
+                        </v-card>
+                    </v-col>
+                    <v-col cols="4">
+                        <v-card class="mx-auto my-12" max-width="374">
+                            <img src="~/assets/card1.jpg" height="250" cover alt="">
+                            <v-card-item>
+                                <v-card-title>
+                                    Features of Chain Medics
+                                </v-card-title>
+                                <v-card-text>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed quod corrupti iste. Ad sunt
+                                    eveniet cupiditate similique dicta quia enim amet placeat, iusto cumque perspiciatis
+                                    deleniti recusandae quibusdam voluptatum excepturi.
+                                </v-card-text>
+                            </v-card-item>
+                        </v-card>
+                    </v-col>
+                </v-row>
+            </div>
+
+            <div>
+                <h2 class="text-center text-uppercase">our team</h2>
+                <v-row>
+                    <v-col>
+                        <v-card class="mx-auto my-12" max-width="374">
+                            <v-avatar size="350">
+                                <v-icon size="200">mdi-account-circle</v-icon>
+                            </v-avatar>
+                            <v-card-item>
+                                <v-card-title class="text-center">
+                                    Name of Member
+                                </v-card-title>
+                                <v-card-subtitle class="text-center">
+                                    Occupation
+                                </v-card-subtitle>
+                                <v-card-text>
+                                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex, hic nobis perferendis
+                                    dolore
+                                    esse saepe vero sit quasi, cum quae in dolores rem laborum maiores possimus? Ipsam
+                                    excepturi
+                                    molestiae facere?
+                                </v-card-text>
+                            </v-card-item>
+                        </v-card>
+                    </v-col>
+                    <v-col>
+                        <v-card class="mx-auto my-12" max-width="374">
+                            <v-avatar size="350">
+                                <v-icon size="200">mdi-account-circle</v-icon>
+                            </v-avatar>
+                            <v-card-item>
+                                <v-card-title class="text-center">
+                                    Name of Member
+                                </v-card-title>
+                                <v-card-subtitle class="text-center">
+                                    Occupation
+                                </v-card-subtitle>
+                                <v-card-text>
+                                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex, hic nobis perferendis
+                                    dolore
+                                    esse saepe vero sit quasi, cum quae in dolores rem laborum maiores possimus? Ipsam
+                                    excepturi
+                                    molestiae facere?
+                                </v-card-text>
+                            </v-card-item>
+                        </v-card>
+                    </v-col>
+                    <v-col>
+                        <v-card class="mx-auto my-12" max-width="374">
+                            <v-avatar size="350">
+                                <v-icon size="200">mdi-account-circle</v-icon>
+                            </v-avatar>
+                            <v-card-item>
+                                <v-card-title class="text-center">
+                                    Name of Member
+                                </v-card-title>
+                                <v-card-subtitle class="text-center">
+                                    Occupation
+                                </v-card-subtitle>
+                                <v-card-text>
+                                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex, hic nobis perferendis
+                                    dolore
+                                    esse saepe vero sit quasi, cum quae in dolores rem laborum maiores possimus? Ipsam
+                                    excepturi
+                                    molestiae facere?
+                                </v-card-text>
+                            </v-card-item>
+                        </v-card>
+                    </v-col>
+                    <v-col>
+                        <v-card class="mx-auto my-12" max-width="374">
+                            <v-avatar size="350">
+                                <v-icon size="200">mdi-account-circle</v-icon>
+                            </v-avatar>
+                            <v-card-item>
+                                <v-card-title class="text-center">
+                                    Name of Member
+                                </v-card-title>
+                                <v-card-subtitle class="text-center">
+                                    Occupation
+                                </v-card-subtitle>
+                                <v-card-text>
+                                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex, hic nobis perferendis
+                                    dolore
+                                    esse saepe vero sit quasi, cum quae in dolores rem laborum maiores possimus? Ipsam
+                                    excepturi
+                                    molestiae facere?
+                                </v-card-text>
+                            </v-card-item>
+                        </v-card>
+                    </v-col>
+                    <v-col>
+                        <v-card class="mx-auto my-12" max-width="374">
+                            <v-avatar size="350">
+                                <v-icon size="200">mdi-account-circle</v-icon>
+                            </v-avatar>
+                            <v-card-item>
+                                <v-card-title class="text-center">
+                                    Name of Member
+                                </v-card-title>
+                                <v-card-subtitle class="text-center">
+                                    Occupation
+                                </v-card-subtitle>
+                                <v-card-text>
+                                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex, hic nobis perferendis
+                                    dolore
+                                    esse saepe vero sit quasi, cum quae in dolores rem laborum maiores possimus? Ipsam
+                                    excepturi
+                                    molestiae facere?
+                                </v-card-text>
+                            </v-card-item>
+                        </v-card>
+                    </v-col>
+                </v-row>
+            </div>
+        </v-container>
     </div>
 </template>
 <style>
 .home {
-    width: 100%;
-    height: 100%;
+    width: 80%;
+    /* height: 80%; */
     /* background: rgba(144, 230, 244, 0.721) url("/assets/bg-img.png") no-repeat cover; */
 }
+
+.spons {
+    background: rgb(213, 213, 211);
+}
+
+.spons-logo {
+    height: 3.5em;
+    width: auto;
+}
 </style>
+<script>
+
+</script>
